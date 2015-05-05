8b4647836aa70dcff1b45f4a4bf7907c425c1d71
Michał <Micha@169.254.80.80>
2015-05-05 23:54:17 +0200

    msg

---
+++ src/carpanel/CarPanel.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carpanel;
+
+/**
+ *
+ * @author Michał
+ */
+public class CarPanel {
+
+    private double s, t;
+    private boolean WARN = false;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public CarPanel()
+    {
+        
+    }
+    
+    public double speed()
+    {
+        return s/t;
+    }
+    
+}
